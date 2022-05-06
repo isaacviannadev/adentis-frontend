9fcf23633b10690ec49894d008ab7b8994ccb5dc
isaacviannadev <isaacsvianna@gmail.com>
2022-05-06 09:30:30 -0300

    refactor: Change date to Portugal Timezone

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,8 +1,10 @@
 export const formatDate = (date: Date): string => {
-  date.setHours(date.getHours() + 3);
+  date.setTime(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
   const day = date.getDate().toPrecision().padStart(2, '0');
   const month = (date.getMonth() + 1).toPrecision().padStart(2, '0');
   const year = date.getFullYear();
+
+  console.log(`${day}/${month}/${year}`);
 
   return `${day}/${month}/${year}`;
 };
