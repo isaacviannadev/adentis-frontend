277d4774bf88a933f63a95185a1ef959270f98ab
isaacviannadev <isaacsvianna@gmail.com>
2022-05-06 09:23:14 -0300

    fix: the function to format date, change the GMT from -3 to 0, adding 3 hours to date

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,7 +1,9 @@
 export const formatDate = (date: Date): string => {
+  date.setHours(date.getHours() + 3);
   const day = date.getDate().toPrecision().padStart(2, '0');
   const month = (date.getMonth() + 1).toPrecision().padStart(2, '0');
   const year = date.getFullYear();
 
+  console.log(day, month, year, date);
   return `${day}/${month}/${year}`;
 };
